a0817033f3b10d58ccd34e194e136eace931638a
052727 <1432945855@qq.com>
2017-06-15 15:54:35 +0800

    1

---
+++ beyond-center-webapp/UserInfoController.java
@@ -0,0 +1,76 @@
+package com.beyond.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONArray;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.beyond.mapper.UserInfoMapper;
+import com.beyond.model.UserInfo;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserInfoController {
+
+    @Autowired
+    private UserInfoMapper userInfoMapper;
+
+    @RequestMapping(value = "/getUserInfoList")
+    public List<UserInfo> getUserInfoList(HttpServletRequest request,
+            HttpServletResponse response, ModelMap modelMap) {
+
+         return userInfoMapper.selectUserInfoList();
+    }
+
+    @RequestMapping(value = "/getUserInfoById")
+    public Map<String, Object> getUserInfoById(HttpServletRequest request,
+            HttpServletResponse response, ModelMap modelMap, String id) {
+
+        if (StringUtils.isNotBlank(id)) {
+            return userInfoMapper.getUserInfoById(Integer.parseInt(id));
+        }
+        return null;
+    }
+
+    @RequestMapping(value = "/editUserInfo")
+    public String editUserInfo(HttpServletRequest request,
+            HttpServletResponse response, ModelMap modelMap, UserInfo userInfo) {
+
+        if (userInfo != null) {
+            if (userInfo.getId() != null) {
+                userInfoMapper.updateUserInfo(userInfo);
+            } else {
+                userInfoMapper.insertUserInfo(userInfo);
+            }
+            return "success";
+        }
+        return "error";
+    }
+
+    @RequestMapping(value = "/deleteUserInfoById")
+    public String deleteUserInfoById(HttpServletRequest request,
+            HttpServletResponse response, ModelMap modelMap, String id) {
+
+        if (StringUtils.isNotBlank(id)) {
+            userInfoMapper.deleteUserInfoById(Integer.parseInt(id));
+            return "success";
+        }
+        return "error";
+    }
+
+    @RequestMapping(value = "/testJsonMicroservice")
+    public String getInfo(HttpServletRequest request,
+            HttpServletResponse response, ModelMap modelMap) {
+    	JSONArray val=userInfoMapper.getInfo();
+         return val.toString();
+    }
+}
